ad9a1873c695e20fcb97cfe8ebd7981a87edd0e0
Caio Maqueise <caio.pinheiro@ifms.edu.br>
2023-08-21 21:57:44 -0400

    first commit

---
+++ src/js/libs.js
@@ -0,0 +1,8 @@
+// Import our custom CSS
+import "../scss/styles.scss"
+// importação dos ícones
+import "bootstrap-icons/font/bootstrap-icons.css"
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap"
+
+export { bootstrap }
