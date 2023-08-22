61c4fc0f58dfda3b0b9a25c6d02c35eab9278839
Caio Maqueise <caio.pinheiro@ifms.edu.br>
2023-08-21 22:18:47 -0400

    selecionando elementos do DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 import "./libs"
+const inputNome = 
+document.querySelector('#input-nome')
+const selectPrioridade = 
+document.querySelector('#select-prioridade')
+const btnIncluir = 
+document.querySelector('#btn-incluir')
+const ulLista = 
+document.querySelector('#ul-lista')
+const displayNome = 
+document.querySelector('#display-nome')
+const btnChamar = 
+document.querySelector('#btn-chamar')
+const listaDeAtendimento = new Array()
